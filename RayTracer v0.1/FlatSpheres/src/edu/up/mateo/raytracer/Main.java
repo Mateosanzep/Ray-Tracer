df6409d50e2380b847675dc1b6571397679e19cc
Mateosanzep <mateosanzep@gmail.com>
2026-04-20 10:14:51 -0600

    square aspect ratio

--- RayTracer v0.1/FlatSpheres/src/edu/up/mateo/raytracer/Main.java
+++ RayTracer v0.1/FlatSpheres/src/edu/up/mateo/raytracer/Main.java
@@ -8,11 +8,11 @@ import javax.imageio.ImageIO;
 
 public class Main {
         public static void main(String[] args) throws IOException {
-        Camera camera = new Camera(new Vector3D(0,0,0), 800, 600, (float)Math.PI/2);
+        Camera camera = new Camera(new Vector3D(0,0,0), 800, 800, (float)Math.PI/2);
         
         Scene scene = new Scene(camera);
 
-        scene.addObject(new Sphere(Color.RED, new Vector3D(-1, -1, -15), 2.0f));
+        scene.addObject(new Sphere(Color.RED, new Vector3D(-1, -2, -15), 2.0f));
         scene.addObject(new Sphere(Color.BLUE, new Vector3D(20, 20, -100), 7.0f));
         
         Raytracer rt = new Raytracer(scene);
